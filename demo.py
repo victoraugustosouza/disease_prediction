035c87829b98e391484384acb5220b9c4641011f
victoraugustosouza <victor_engenharia@poli.ufrj.br>
2019-06-23 22:37:57 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,7 +11,8 @@ from joblib import dump, load
 clf = load('filename.joblib') 
 
 entradas=[]
-print("Digite o valor das features na mesma ordem do dataset disponibilizado utilizando virgula como separador e aperte enter. Quando terminar digite 'sair'. \n ")
+print("Digite o valor das features na mesma ordem do dataset disponibilizado utilizando virgula como separador e aperte enter. Quando terminar digite 'sair'.")
+print('Exemplo de entrada: 1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 \n ')
 var =input("Digite o valor das features: ")
 while(var != 'sair'):
     
